638ed52e58586d65c6466e29ffe02669b4bdf9a5
chernyshev-alexander <achernyshev@griddynamics.com>
2025-02-26 21:47:26 +0100

    999

--- ToDo_List.py
+++ ToDo_List.py
@@ -3,7 +3,7 @@ from datetime import date
 # masha changes
 
 class User:
-    max_tasks_renamed = 10000  #  was 10
+    max_tasks_renamed = 9999  #  was 10
 
     def __init__(self, login, username, password):
         """
